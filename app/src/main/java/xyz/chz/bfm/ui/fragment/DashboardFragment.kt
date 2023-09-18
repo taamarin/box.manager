353bc218b90be532e7872d664261c604d12d8ede
riffchz <riffchz@gmail.com>
2023-09-18 15:41:39 +0700

    add dasboard

---
+++ app/src/main/java/xyz/chz/bfm/ui/fragment/DashboardFragment.kt
@@ -0,0 +1,44 @@
+package xyz.chz.bfm.ui.fragment
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import xyz.chz.bfm.databinding.FragmentDashboardBinding
+import xyz.chz.bfm.util.command.ThermUtil
+
+@AndroidEntryPoint
+class DashboardFragment : Fragment() {
+
+    private lateinit var binding: FragmentDashboardBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDashboardBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            dbWebview.loadUrl("${ThermUtil.linkDasboard()}/ui/#/proxies")
+            with(dbWebview.settings) {
+                domStorageEnabled = true
+                databaseEnabled = true
+                allowContentAccess = true
+                javaScriptEnabled = true
+                cacheMode = WebSettings.LOAD_NO_CACHE
+                dbWebview.webViewClient = WebViewClient()
+            }
+        }
+    }
+
+}
--- app/src/main/java/xyz/chz/bfm/util/command/ThermUtil.kt
+++ app/src/main/java/xyz/chz/bfm/util/command/ThermUtil.kt
@@ -4,25 +4,31 @@ import xyz.chz.bfm.util.magisk.MagiskHelper
 import kotlin.concurrent.thread
 
 object ThermUtil {
+    val path = "/data/adb/box/"
+
     fun isProxying(): Boolean {
-        return MagiskHelper.execRootCmdSilent("if [ -f /data/adb/box/run/box.pid ] ; then exit 1;fi") == 1
+        return MagiskHelper.execRootCmdSilent("if [ -f ${path}run/box.pid ] ; then exit 1;fi") == 1
     }
 
     fun start(callback: (Boolean) -> Unit) {
         thread {
-            val cmd = "/data/adb/box/scripts/box.service start && /data/adb/box/scripts/box.iptables enable"
+            val cmd = "${path}scripts/box.service start && ${path}scripts/box.iptables enable"
             MagiskHelper.execRootCmdVoid(cmd, callback)
         }
     }
 
     fun stop(callback: (Boolean) -> Unit) {
         thread {
-            val cmd = "/data/adb/box/scripts/box.iptables disable && /data/adb/box/scripts/box.service stop"
+            val cmd = "${path}scripts/box.iptables disable && ${path}scripts/box.service stop"
             MagiskHelper.execRootCmdVoid(cmd, callback)
         }
     }
 
     fun readLog(): String {
-        return MagiskHelper.execRootCmd("cat /data/adb/box/run/runs.log")
+        return MagiskHelper.execRootCmd("cat ${path}run/runs.log")
+    }
+
+    fun linkDasboard(): String {
+        return MagiskHelper.execRootCmd("grep 'external-controller:' ${path}clash/config.yaml | awk '{print $2}'")
     }
 }
